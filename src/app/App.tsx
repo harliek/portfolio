bed08270dbf368237c5a2ea4c1ce72003a5c0c23
Harlie <harliekatz@berkeley.edu>
2026-08-04 20:35:08 -0700

    Fix portfolio page routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -170,7 +170,7 @@ function Background() {
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Background />
 
       <div className="relative z-10 min-h-screen">
